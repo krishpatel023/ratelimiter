503dcb5b9909c82fe10f5ba31e5004fbd80bad6c
krishpatel023 <krishpatel0234@gmail.com>
2025-03-03 22:22:09 +0530

    add: reverse proxy

---
+++ limiters/reverse-proxy.go
@@ -0,0 +1,30 @@
+package limiters
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"os"
+
+	"github.com/krishpatel023/ratelimiter/internal/helper"
+)
+
+func reverseProxy(TargetURL string) http.Handler {
+	// Define the backend server URL
+	targetURL, err := url.Parse(TargetURL)
+	if err != nil || targetURL.String() == "" {
+		helper.Log(fmt.Sprintf("Failed to parse target URL: %v", "Please add/check the target URL"), "error")
+		os.Exit(1)
+	}
+
+	// Create a reverse proxy
+	proxy := httputil.NewSingleHostReverseProxy(targetURL)
+
+	// Set up the handler
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		proxy.ServeHTTP(w, r)
+	})
+
+	return handler
+}
