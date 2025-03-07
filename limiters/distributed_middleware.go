afe54d04e14e73e75a0ed52bd01ea313b56a1859
krishpatel023 <krishpatel0234@gmail.com>
2025-03-07 13:15:27 +0530

    fix: distributed middleware
    Added Redis connection check to verify that the connection has been established f=befor the server starts

--- limiters/distributed_middleware.go
+++ limiters/distributed_middleware.go
@@ -1,10 +1,13 @@
 package limiters
 
 import (
+	"context"
 	"net/http"
+	"time"
 
 	"github.com/krishpatel023/ratelimiter/internal/helper"
 	rate_limiter "github.com/krishpatel023/ratelimiter/internal/rate-limiter"
+	"github.com/redis/go-redis/v9"
 )
 
 // Distributed Rate Limiter Middleware
@@ -23,6 +26,13 @@ func DistributedRateLimitingMiddleware(rl *rate_limiter.DistributedRateLimiter, 
 	if config.UniqueHeaderNameInRequest == "" {
 		helper.Log("Request rejected: Set UniqueHeaderNameInRequest header in config", "warning")
 		http.Error(nil, "Set UniqueHeaderNameInRequest header in config", http.StatusBadRequest)
+		return nil
+	}
+
+	// Check if the redis connection is working
+	connection, err := RedisCheck(config.RedisDBAddress, config.RedisDBPassword, config.StorageDB)
+	if !connection || err != nil {
+		helper.Log("Request rejected: Redis connection failed", "warning")
 		return nil
 	}
 
@@ -50,4 +60,32 @@ func DistributedRateLimitingMiddleware(rl *rate_limiter.DistributedRateLimiter, 
 		helper.Log("Request allowed - RequestID: "+requestID, "info")
 		handler.ServeHTTP(w, r)
 	})
+}
+
+func RedisCheck(redisAddr, password string, db int) (bool, error) {
+
+	// Create Redis client
+	client := redis.NewClient(&redis.Options{
+		Addr:     redisAddr,
+		Password: password,
+		DB:       db,
+	})
+
+	// Test connection
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// Wait for Redis to be ready
+	// Requesting the ping command for at least 3 times
+	// with a 2 second interval
+	var err error
+	for i := 0; i < 3; i++ {
+		err = client.Ping(ctx).Err()
+		if err == nil {
+			return true, nil
+		}
+		time.Sleep(2 * time.Second)
+	}
+
+	return false, err
 }
